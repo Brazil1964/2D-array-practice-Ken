171523750453dac59b3217c276a3eda1318af05c
Ken <kenhuangsy@gmail.com>
2023-01-10 14:11:43 -0800

    I used a hash map for the last one because I am used to doing that in C++ lol

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,15 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 public class MyMain {
 
     // Returns the number of odd numbers in mat
     public static int countOdds(int[][] mat) {
-        // YOUR CODE HERE
-        return -1;
+        int counter = 0;
+        for (int[]arr : mat){
+            for (int i : arr){
+                if (i % 2 == 1) counter++;
+            }
+        }
+        return counter;
     }
 
     // Returns the largest value in mat
     public static int findLargest(int[][] mat) {
-        // YOUR CODE HERE
-        return -1;
+       int max_int = Integer.MIN_VALUE;
+       for (int [] arr : mat){
+           for (int i : arr){
+               max_int = Math.max(max_int, i);
+           }
+       }
+       return max_int;
     }
 
     // Write a method that "left-shifts" each row in a 2D array.
@@ -17,15 +29,25 @@ public class MyMain {
     // shifted to the left by 1, with wraparound (the first value
     // should wrap around to the last entry in the row)
     public static int[][] leftShift(int[][] mat) {
-        // YOUR CODE HERE
-        return null;
+        int [][] new_mat = new int[mat.length][mat[0].length];
+        for (int i = 0; i < mat.length; i++){
+            for (int j = 0; j < mat[0].length; j++){
+                new_mat[i][j] = mat[i][(j+1) % mat[0].length];
+            }
+        }
+        return new_mat;
     }
 
     // Creates a 2D array of size arraySize x arraySize
     // where each value is each to the sum of its row and col indexes
     public static int[][] arrayBuilder(int arraySize) {
-        // YOUR CODE HERE
-        return null;
+        int [][] two_arr = new int[arraySize][arraySize];
+        for (int i = 0; i < arraySize; i++){
+            for (int j = 0; j < arraySize; j++){
+                two_arr[i][j] = i+j;
+            }
+        }
+        return two_arr;
     }
 
 
@@ -41,23 +63,57 @@ public class MyMain {
 
     // Returns the mean of the 2D array mat
     public static double mean(double[][] mat) {
-        // YOUR CODE HERE
-        return -1.0;
+        double current_sum = 0;
+        for (double [] arr : mat){
+            for (double i : arr){
+                current_sum += i;
+            }
+        }
+        return current_sum/(mat.length * mat[0].length);
     }
 
     // Returns the median of the 2D array mat
     // Remember that the array is sorted!
     public static double median(double[][] mat) {
-        // YOUR CODE HERE
-        return -1.0;
+        ArrayList<Double>temp_arr = new ArrayList<Double>();
+        for (double [] arr : mat){
+            for (double i : arr){
+                temp_arr.add(i);
+            }
+        }
+        if (temp_arr.size() % 2 == 0){
+            return (temp_arr.get(temp_arr.size()/2) + temp_arr.get(temp_arr.size()/2 - 1))/2;
+        }
+        else{
+            return temp_arr.get(temp_arr.size()/2);
+        }
     }
 
 
     // Returns the mode of the 2D array mat
     // Remember that the array is sorted!
+
     public static double mode(double[][] mat) {
-        // YOUR CODE HERE
-        return -1.0;
+        HashMap<Double, Integer>temp_map = new HashMap<Double, Integer>();
+        for (double [] arr : mat){
+            for (double i : arr){
+                if (temp_map.containsKey(i)){
+                    temp_map.put(i, temp_map.get(i) + 1);
+                }
+                else{
+                    temp_map.put(i, 1);
+                }
+            }
+        }
+        double max_key = 0;
+        int max_val = 0;
+        for (double key : temp_map.keySet()){
+            if (temp_map.get(key) > max_val){
+                max_val = temp_map.get(key);
+                max_key = key;
+            }
+        }
+        return max_key;
     }
 
 
